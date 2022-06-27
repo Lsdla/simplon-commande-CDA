6899a8fdaa42610dc4229ba3c71a8194a02729de
Lsdla <al3s2@yahoo.fr>
2022-06-27 11:08:26 +0200

    2)- import du module core qui lui export certains module pour des questions de performances

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {UiModule} from "../ui/ui.module";
+import {IconsModule} from "../icons/icons.module";
+import {TemplatesModule} from "../templates/templates.module";
+import {LoginModule} from "../login/login.module";
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    UiModule,
+    IconsModule,
+    TemplatesModule,
+    LoginModule
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+}
