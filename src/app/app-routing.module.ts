5222a8533ec714e864b52750c221fe18cb2cb6fc
Lsdla <al3s2@yahoo.fr>
2022-06-27 17:43:24 +0200

    11)- preloading strategy: les modules qui sont en lazyLoading ne sont chargés qu'après rechargement du core et s'il reste de la bande passante et si la cpu baisse il charge le preloading (les loadChildren)
    dans app-routing:
    
    @NgModule({
      imports: [
        RouterModule.forRoot(
          routes,
          { preloadingStrategy: PreloadAllModules }
        ),
      ],

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {PageSignInComponent} from "./login/pages/page-sign-in/page-sign-in.component";
 import {PageSignUpComponent} from "./login/pages/page-sign-up/page-sign-up.component";
 import {PageResetPasswordComponent} from "./login/pages/page-reset-password/page-reset-password.component";
@@ -26,7 +26,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      { preloadingStrategy: PreloadAllModules }
+    ),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
