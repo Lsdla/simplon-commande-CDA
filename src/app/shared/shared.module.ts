b62cd1bd1181da0257c6a2544a3e7ba03e19ca77
Lsdla <al3s2@yahoo.fr>
2022-06-29 18:04:01 +0200

    27)- Btn component & ready to (re)use:
    lien vers différentes directives :
    https://stackblitz.com/edit/angular-ivy-renderer2-directive-xwmqx9?file=src%2Fapp%2Fapp.component.html,src%2Fapp%2Fcp1.directive.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { BtnComponent } from './components/btn/btn.component';
 import { TableLightComponent } from './components/table-light/table-light.component';
 import { TotalPipe } from './pipes/total.pipe';
 import { StateDirective } from './directives/state.directive';
+import {RouterLink, RouterModule} from "@angular/router";
 
 
 
@@ -16,7 +17,8 @@ import { StateDirective } from './directives/state.directive';
     StateDirective
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     TemplatesModule,
